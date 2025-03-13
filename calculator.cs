28799edf3d3472dd76a47118673fb43f8d038acc
affanqhstaaa <affanqhastalany@students.amikom.ac.id>
2025-03-13 11:26:51 +0700

    perubahan'

--- calculator.cs
+++ calculator.cs
@@ -2,6 +2,20 @@ class calculator
 {
 		static void Main(string[])
 		{
+			int a= 10;
+			int b= 6;
+			Console.Writeline("Hasil Penambahan: {0} + {1}",a,b,Penambahan(a,b));
+			Console.Writeline("Hasil Pengurangan: {0} - {1}",a,b,Penambahan(a,b));
 			
+			Console.WriteLine("\ntekan sembarang key untuk keluar");
+			Console.Readkey();
+		}
+		static int Penambahan(int a,int b)
+		{
+			return a + b;
+		}
+		static int Pengurangan(int a,int b)
+		{
+			return a - b;
 		}
 }
